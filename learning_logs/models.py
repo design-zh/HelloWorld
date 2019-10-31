a5fc89667f89b13ad895aa8480a030accc306817
ZH <zh@ZHdeMacBook-Air.local>
2019-10-31 18:56:44 +0800

    注释语句

---
+++ learning_logs/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+    """用户学习的主题"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def _str_(self):
+        return self.text
